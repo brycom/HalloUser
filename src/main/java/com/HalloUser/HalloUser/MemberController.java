f0b5e687afdf4bca86f4bf4f20ffc1a17c382e4b
brycom <mathiasbrynolf20@gmail.com>
2023-10-30 12:41:09 +0100

    method for new member is done

--- src/main/java/com/HalloUser/HalloUser/MemberController.java
+++ src/main/java/com/HalloUser/HalloUser/MemberController.java
@@ -1,20 +1,34 @@
 package com.HalloUser.HalloUser;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class MemberController {
 
   @GetMapping("/new-member")
-  public String GetNewMember() {
+  public String GetNewMember(Model model) {
+    model.addAttribute("newMember", new Member(null, null, null, 0, 0));
+    System.out.println();
     return "AddMember";
   }
 
   @PostMapping("/add-new-member")
-  String newMember() {
-    System.out.println("Det fungerar!!!");
+  String newMember(
+    @RequestParam("firstName") String firstName,
+    @RequestParam("lastName") String lastName,
+    @RequestParam("city") String city,
+    @RequestParam("yearsActive") int yearsActive
+  ) {
+    HalloUserApplication.clubb.addMember(
+      firstName,
+      lastName,
+      city,
+      yearsActive
+    );
 
     return "redirect:new-member";
   }
