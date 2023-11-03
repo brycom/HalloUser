d1f425df347e2cf31e84fe42a9ca087f89049b88
brycom <mathiasbrynolf20@gmail.com>
2023-11-03 16:12:12 +0100

    färdig

--- src/main/java/com/HalloUser/HalloUser/ClubbController.java
+++ src/main/java/com/HalloUser/HalloUser/ClubbController.java
@@ -1,5 +1,6 @@
 package com.HalloUser.HalloUser;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,8 +9,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class ClubbController {
 
   @GetMapping("/member-list")
-  public String GetMemberList(Model model) {
+  public String GetMemberList(Model model, HttpServletRequest request) {
     model.addAttribute("members", HalloUserApplication.clubb.members);
+
+    Object errorMessage = model.asMap().get("error-message");
+    model.addAttribute("error", errorMessage);
 
     return "MemberList";
   }
